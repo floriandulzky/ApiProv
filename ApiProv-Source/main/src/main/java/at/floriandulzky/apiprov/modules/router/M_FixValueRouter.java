2c45472d588e091f5a340e8d7d09e4c2ebda3bbf
Florian <Florian.Dulzky@nts.eu>
2020-03-29 14:35:57 +0200

    M_RestRouter and M_FixValueRouter implemented

---
+++ ApiProv-Source/main/src/main/java/at/floriandulzky/apiprov/modules/router/M_FixValueRouter.java
@@ -0,0 +1,22 @@
+package at.floriandulzky.apiprov.modules.router;
+
+import at.floriandulzky.apiprov.plugin.router.Router;
+import at.floriandulzky.apiprov.plugin.router.exceptions.RouterException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class M_FixValueRouter implements Router {
+
+    @Override
+    public Object handle(Map<String, List<String>> httpHeaders, Map<String, List<String>> queryParams, String body,
+                         String method, Properties properties, String uri) throws RouterException {
+        return properties.get("data").toString();
+    }
+
+    @Override
+    public String getName() {
+        return "fix value";
+    }
+}
--- ApiProv-Source/main/src/main/java/at/floriandulzky/apiprov/modules/router/M_RestRouter.java
+++ ApiProv-Source/main/src/main/java/at/floriandulzky/apiprov/modules/router/M_RestRouter.java
@@ -2,8 +2,13 @@ package at.floriandulzky.apiprov.modules.router;
 
 import at.floriandulzky.apiprov.plugin.router.Router;
 import at.floriandulzky.apiprov.plugin.router.exceptions.RouterException;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
-import java.util.HashMap;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -13,10 +18,21 @@ public class M_RestRouter implements Router {
     @Override
     public Object handle(Map<String, List<String>> httpHeaders, Map<String, List<String>> queryParams,
                                String body, String method, Properties properties, String uri) throws RouterException {
-        Map<String, String> hashMap = new HashMap<>();
-        hashMap.put("test", "das ist ein Test");
-        hashMap.put("test2", "Das ist Test 2");
-        return hashMap;
+        Client resteasyClient =  ResteasyClientBuilder.newBuilder().build();
+        WebTarget target = resteasyClient.target(properties.getProperty("uri"));
+        for(Map.Entry<String, List<String>> queryParam : queryParams.entrySet()){
+            target = target.queryParam(queryParam.getKey(), queryParam.getValue());
+        }
+        Invocation.Builder request = target.request();
+        for(Map.Entry<String, List<String>> httpHeader : httpHeaders.entrySet()){
+            for(String value : httpHeader.getValue()){
+                request.header(httpHeader.getKey(), value);
+            }
+        }
+        if(method != "GET")
+            return request.build(method, Entity.entity(body, MediaType.TEXT_PLAIN)).submit(String.class);
+        else
+            return request.buildGet().invoke(String.class);
     }
 
     @Override
