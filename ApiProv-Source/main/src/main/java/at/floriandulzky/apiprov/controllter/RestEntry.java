25e78cc53d1461f733bc47a582e243f81157c378
Florian <Florian.Dulzky@nts.eu>
2020-03-29 14:57:41 +0200

    REST MediaType.TEXT_PLAIN implemented

--- ApiProv-Source/main/src/main/java/at/floriandulzky/apiprov/controllter/RestEntry.java
+++ ApiProv-Source/main/src/main/java/at/floriandulzky/apiprov/controllter/RestEntry.java
@@ -7,7 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 @Path("/")
-public class RestJson {
+public class RestEntry {
 
     @Inject
     AppControllerService appControllerService;
@@ -26,10 +26,24 @@ public class RestJson {
         return appControllerService.handleRequest(path, this.uriInfo, this.headers, "GET", body);
     }
 
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/t/{path: (.*)}")
+    public Response getPlainText(@PathParam("path") String path, String body) {
+        return appControllerService.handleRequest(path, this.uriInfo, this.headers, "GET", body);
+    }
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{path: (.*)}")
     public Response post(@PathParam("path") String path, @Context UriInfo uriInfo, String body) {
+        return appControllerService.handleRequest(path, this.uriInfo, this.headers, "POST", body);
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/t/{path: (.*)}")
+    public Response postPlainText(@PathParam("path") String path, @Context UriInfo uriInfo, String body) {
         return appControllerService.handleRequest(path, this.uriInfo, this.headers, "POST", body);
     }
 
@@ -40,10 +54,24 @@ public class RestJson {
         return appControllerService.handleRequest(path, this.uriInfo, this.headers, "DELETE", body);
     }
 
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/t/{path: (.*)}")
+    public Response deletePlaintext(@PathParam("path") String path, @Context UriInfo uriInfo, String body) {
+        return appControllerService.handleRequest(path, this.uriInfo, this.headers, "DELETE", body);
+    }
+
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{path: (.*)}")
     public Response put(@PathParam("path") String path, @Context UriInfo uriInfo, String body) {
+        return appControllerService.handleRequest(path, this.uriInfo, this.headers, "PUT", body);
+    }
+
+    @PUT
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/t/{path: (.*)}")
+    public Response putPlainText(@PathParam("path") String path, @Context UriInfo uriInfo, String body) {
         return appControllerService.handleRequest(path, this.uriInfo, this.headers, "PUT", body);
     }
 
